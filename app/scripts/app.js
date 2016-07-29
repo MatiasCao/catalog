ced0059d5833e2c8fe3e7baba0c914b55ce5d0cc
Lucas Nuñez <lucas.nunez@globant.com>
2016-07-29 12:03:44 -0300

    Project Setup

---
+++ app/scripts/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name catalogApp
+ * @description
+ * # catalogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('catalogApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/product-detail', {
+        templateUrl: 'views/product-detail.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/dashboard/add', {
+        templateUrl: 'views/dashboard/add.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/dashboard/edit', {
+        templateUrl: 'views/dashboard/edit.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/dashboard/trash', {
+        templateUrl: 'views/dashboard/trash.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
